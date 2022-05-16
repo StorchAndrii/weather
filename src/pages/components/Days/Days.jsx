c6683dc2261337967513c1be83e7d2cb0c042a9f
StorchAndrii <andrey.st.light@gmail.com>
2022-05-16 23:24:31 +0300

    add DayJS(ru)

--- src/pages/components/Days/Days.jsx
+++ src/pages/components/Days/Days.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import dayjs from "dayjs";
+import "dayjs/locale/ru";
 
 import s from "./Days.module.scss";
 import { Card } from "./Card";
@@ -11,7 +12,6 @@ export const Days = () => {
   const day = weather.forecast.forecastday;
   const days = day.map((day) => ({
     dayCard: dayjs(day.date).locale("ru").format("D  MMM"),
-    // а зачем два поля с одинаковой информацией?
     iconId: day.day.condition.icon,
     tempDay: Math.floor(day.day.maxtemp_c),
     tempNight: Math.floor(day.day.mintemp_c),
