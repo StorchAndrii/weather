b07182619718ca449c043022282f39658a5feffd
StorchAndrii <andrey.st.light@gmail.com>
2022-05-25 11:55:36 +0300

    fin

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,6 @@ import Header from "./components/Header/Header";
 function App() {
   return (
     <div>
-      {/*<Routes>*/}
-      {/*  <Route path="/info/:dayCard" element={<Popup />} />*/}
-      {/*</Routes>*/}
       <div className="container">
         <Header />
         <Home />
